669d718236d5d7f38399ad80fcd7e8311f40e811
DexRam <Ramil.Budagli@yandex.ru>
2024-07-01 16:43:46 +0400

    first commit

---
+++ API/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    fullname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    isAdmin = models.BooleanField(default=False)
+    filepath = models.CharField(max_length=100, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.password:
+            self.password = make_password(self.password)
+        if not self.filepath:
+            self.filepath = self.username
+        super().save(*args, **kwargs)
+    
+    def check_password(self, password):
+        return check_password(password, self.password)
+
+class File(models.Model):
+    name = models.CharField(max_length=100)
+    path = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
