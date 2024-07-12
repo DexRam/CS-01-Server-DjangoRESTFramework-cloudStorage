c8661d83ca1a8097aec1f4aaec2181c9f1fa053d
DexRam <Ramil.Budagli@yandex.ru>
2024-07-12 16:43:36 +0400

    Update and refactoring

--- API/models.py
+++ API/models.py
@@ -3,23 +3,35 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 
+
 class User(AbstractUser):
     fullname = models.CharField(max_length=100)
-    isAdmin = models.BooleanField(default=False)
-    filepath = models.CharField(max_length=100, blank=True, editable=False)
+    is_admin = models.BooleanField(default=False)
+    file_path = models.CharField(max_length=100, blank=True, editable=False)
+    is_active = models.BooleanField(default=True)
 
     def save(self, *args, **kwargs):
-        self.filepath = f"files/{self.username}/"
+        self.file_path = os.path.join("files", self.username)
+        if self.is_superuser:
+            self.is_admin = True
         super().save(*args, **kwargs)
-    
 
 
 class File(models.Model):
-    name = models.CharField(max_length=100)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='files', to_field='username', blank=True,)
-    file = models.FileField(upload_to='files/')
+    name = models.CharField(max_length=100, unique=True)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="files",
+        to_field="username",
+        blank=True,
+    )
     uploaded_at = models.DateTimeField(default=timezone.now)
+    downloaded_at = models.DateTimeField(blank=True, null=True)
+    share_link = models.CharField(max_length=100, blank=True)
+    comment = models.CharField(max_length=100, blank=True)
 
-    def save(self, *args, **kwargs):
-        self.file.name = os.path.join(self.owner.filepath, self.file.name)
-        super().save(*args, **kwargs)
+    def file_upload_path(self, filename):
+        return f"files/{self.owner.username}/{filename}"
+
+    file = models.FileField(upload_to=file_upload_path)
--- API/views.py
+++ API/views.py
@@ -1,3 +1,4 @@
+from django.http import FileResponse
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -5,7 +6,9 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.authentication import SessionAuthentication
+from rest_framework.generics import get_object_or_404
 
+from .permissions import IsAdminUser
 from .models import User, File
 from .serializers import UserSerializer, FileSerializer
 
@@ -14,19 +17,19 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     authentication_classes = [JWTAuthentication, SessionAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAdminUser]
 
     def get_permissions(self):
-        if self.action == 'create' or self.action == 'login' :
+        if self.action == "create" or self.action == "login":
             self.permission_classes = [AllowAny]
         return super().get_permissions()
-    
+
     def create(self, request, *args, **kwargs):
         serializer = UserSerializer(data=request.data)
         print(request.data)
         if serializer.is_valid():
             user = serializer.save()
-            user.set_password(request.data['password'])
+            user.set_password(request.data["password"])
             user.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
@@ -38,15 +41,16 @@ class UserViewSet(viewsets.ModelViewSet):
         password = request.data.get("password")
         user = User.objects.filter(username=username).first()
         if not user or not user.check_password(password):
-            return Response({"error": "Invalid credentials"},
-                            status=status.HTTP_401_UNAUTHORIZED)
+            return Response(
+                {"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED
+            )
         refresh = RefreshToken.for_user(user)
         serializer = self.get_serializer(user)
         data = serializer.data
         data.update({"refresh": str(refresh), "access": str(refresh.access_token)})
         return Response(data, status=status.HTTP_200_OK)
-    
-    @action(detail=False, methods=["get"])
+
+    @action(detail=False, methods=["get"], permission_classes=[IsAuthenticated])
     def me(self, request):
         serializer = self.get_serializer(request.user)
         return Response(serializer.data)
@@ -58,24 +62,39 @@ class FileViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
+        print(request.data)
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             serializer.save(owner=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
     def delete(self, *args, **kwargs):
         self.file.delete()
         super().delete(*args, **kwargs)
 
     @action(detail=False, methods=["post"])
     def userFiles(self, request):
-        print(request.user)
-        user = self.request.user
-        if user:
-            files = File.objects.filter(owner=user)
-            serializer = self.get_serializer(files, many=True)
-            return Response(serializer.data)
-        return Response({"error": "Invalid credentials"},
-                        status=status.HTTP_401_UNAUTHORIZED)
+        request_user = self.request.user
+        id = request.data.get("id", None)
+        if request_user.is_admin and id is not None:
+            user = get_object_or_404(User, id=id)
+        else:
+            user = request_user
+        files = File.objects.filter(owner=user)
+        serializer = self.get_serializer(files, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=["get"])
+    def download(self, request, pk=None):
+        file_instance = self.get_object()
+        if request.user == file_instance.owner or request.user.is_admin:
+            file_path = file_instance.file.path
+            with open(file_path, "rb") as file:
+                return FileResponse(file, as_attachment=True)
+        else:
+            return Response(
+                {"Error": "You do not have permission to access this file."},
+                status=status.HTTP_403_FORBIDDEN,
+            )
