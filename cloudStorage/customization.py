c5195a0e4e41ca2adaafc34ac803894a2dbb62b9
DexRam <Ramil.Budagli@yandex.ru>
2024-07-20 21:48:21 +0400

    Update

--- cloudStorage/customization.py
+++ cloudStorage/customization.py
@@ -10,23 +10,23 @@ SECRET_KEY = "django-insecure-zordtkukn0kt16)h^g&t7(vb%^1xk2zkj*^u9l0xw8uvt4g*+%
 DEBUG = True
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "mydatabase",
-#         "USER": "mydatabaseuser",
-#         "PASSWORD": "mypassword",
-#         "HOST": "127.0.0.1",
-#         "PORT": "5432",
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "cloudStorage",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
 
 
 CORS_ALLOWED_ORIGINS = [
